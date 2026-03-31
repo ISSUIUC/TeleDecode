2a2d5a9ed6206358bb57a88cbe4e8e2c12d9840c
Muhammad Ali <muhammadali82090@gmail.com>
2026-03-31 18:41:04 -0500

    init_setup_files

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,14 @@
 #include <Arduino.h>
+#include "cc112x_spi.h"
+#include "pins.h"
+#include "cc1120_config.h"
+
+/*
+ * TeleDecode Board Software
+ * Benson Zhou
+ * Jennifer Luo
+ * Muhammad Ali
+ */
 
 // put function declarations here:
 int myFunction(int, int);
