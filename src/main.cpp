b65b92d1c3ef7cc874cba09291d9580bdc4a7cbf
Zyun Lam <zyunlam@gmail.com>
2026-04-21 19:48:32 -0500

    Add threads

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 #include <SPI.h>
+
 #include "cc1120_config.h"
 #include "pins.h"
+#include "hal.h"
 
 CC1120 radio(SPI, SPI_RADIO_CS, GPIO_RADIO_INT);
 
@@ -14,12 +16,26 @@ void init_gpio() {
   pinMode(SPI_RADIO_CS, OUTPUT);
 }
 
+DECLARE_THREAD(radio, void* nothing) {
+  // Read all our data and stuff  
+}
+
+
+DECLARE_THREAD(usb_output, void* nothing) {
+  // Write and read to console
+}
 
 void setup() {
   digitalWrite(GPIO_LED_RED, HIGH);
   SPI.begin(SPI_RADIO_SCLK, SPI_RADIO_MISO, SPI_RADIO_MOSI);
+  START_THREAD(radio, SENSOR_CORE, NULL, 8);
+  START_THREAD(usb_output, DATA_CORE, NULL, 8);
+  while (true) {
+    THREAD_SLEEP(1000);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Nah
+  // This should never happen
 }
